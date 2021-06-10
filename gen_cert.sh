0c3f1cc1c470c8ea34fcd7335b78040c019de3cb
Pyry Kontio <pyry.kontio@drasa.eu>
2021-06-10 22:32:13 +0900

    Get rid of drasa.eu certs because we use LetsEncrypt for outward-facing stuff

--- gen_cert.sh
+++ gen_cert.sh
@@ -2,17 +2,6 @@
 
 SERIAL=02
 
-cat << EOF > cert/drasa.eu.cfg
-subjectAltName = DNS:drasa.eu, DNS:*.drasa.eu
-EOF
-openssl ecparam -genkey -name prime256v1 -out cert/drasa.eu.key
-openssl req -new -key cert/drasa.eu.key -outform PEM -keyform PEM  -sha256 -out cert/drasa.eu.csr -subj "/C=JP/ST=Tokyo/O=Pyry Kontio/CN=drasa.eu"
-openssl x509 -req -in cert/drasa.eu.csr -sha256 -CA cert/inca.pem -CAkey cert/inca.key -set_serial "0x${SERIAL}1" -days 730 -extfile cert/drasa.eu.cfg -out cert/drasa.eu.pem
-cat cert/drasa.eu.pem > cert/drasa.eu.chain.pem
-cat cert/inca.pem >> cert/drasa.eu.chain.pem
-cat cert/rootca.pem >> cert/drasa.eu.chain.pem
-
-
 cat << EOF > cert/mon.lan.cfg
 subjectAltName = DNS:mon.lan, DNS:mon
 EOF
