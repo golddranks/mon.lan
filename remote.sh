c2ed4f0e1307c3c60f8c7126faefd1e595664ecb
Pyry Kontio <pyry.kontio@drasa.eu>
2022-02-10 22:40:54 +0900

    Enable 802.11r roaming

--- remote.sh
+++ remote.sh
@@ -68,11 +68,23 @@ uci set wireless.default_radio0.key="$WIFI_PW"
 uci set wireless.default_radio0.encryption='psk2'
 uci set wireless.radio0.disabled='0'
 uci set wireless.radio0.country='JP'
+uci set wireless.default_radio0.ieee80211r='1'
+uci set wireless.default_radio0.mobility_domain='cc66'
+uci set wireless.default_radio0.ft_over_ds='1'
+uci set wireless.default_radio0.ft_psk_generate_local='1'
+uci set wireless.radio0.cell_density='0'
+
 uci set wireless.default_radio1.ssid='Skeletor 2.5Ghz'
 uci set wireless.default_radio1.key="$WIFI_PW"
 uci set wireless.default_radio1.encryption='psk2'
 uci set wireless.radio1.disabled='0'
 uci set wireless.radio1.country='JP'
+uci set wireless.default_radio1.ieee80211r='1'
+uci set wireless.default_radio1.mobility_domain='cc66'
+uci set wireless.default_radio1.ft_over_ds='1'
+uci set wireless.default_radio1.ft_psk_generate_local='1'
+uci set wireless.radio1.cell_density='0'
+
 uci commit wireless
 
 # General system settings
