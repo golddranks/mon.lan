867bd9cac9d4a8ecbc2d535285aa234057449861
Pyry Kontio <pyry.kontio@drasa.eu>
2022-02-13 05:12:44 +0900

    Added mwan3

--- remote.sh
+++ remote.sh
@@ -514,5 +514,75 @@ echo "net.ipv6.conf.all.proxy_ndp = 1" > /etc/sysctl.conf
 
 echo "Wireguard settings done."
 
+# MWAN3 Multi-WAN load balancer
+# NOTE: the ORDER of the traffic rules is important, they are processed in top-down order
+
+opkg install mwan3 luci-app-mwan3
+uci import mwan3 << EOF
+config globals 'globals'
+	option mmx_mask '0x3F00'
+
+config interface 'wan'
+	option enabled '1'
+	list track_ip '8.8.4.4'
+	list track_ip '8.8.8.8'
+	list track_ip '208.67.222.222'
+	list track_ip '208.67.220.220'
+	option reliability '2'
+	option family 'ipv4'
+
+config interface 'wan2'
+	option enabled '1'
+	list track_ip '8.8.4.4'
+	list track_ip '8.8.8.8'
+	list track_ip '208.67.222.222'
+	list track_ip '208.67.220.220'
+	option reliability '2'
+	option family 'ipv4'
+
+config member 'wan_w1'
+	option interface 'wan'
+	option metric '1'
+	option weight '1'
+
+config member 'wan2_w5'
+	option interface 'wan2'
+	option metric '1'
+	option weight '5'
+
+config policy 'wan_only'
+	list use_member 'wan_w1'
+
+config policy 'wan2_only'
+	list use_member 'wan2_w5'
+
+config policy 'balanced'
+	list use_member 'wan_w1'
+	list use_member 'wan2_w5'
+
+config rule 'https'
+	option sticky '1'
+	option dest_port '443'
+	option proto 'tcp'
+	option use_policy 'balanced'
+	option family 'ipv4'
+
+config rule 'wan_dns'
+	option dest_ip '210.147.235.3,133.205.66.51'
+	option family 'ipv4'
+	option use_policy 'wan_only'
+
+config rule 'wan2_dns'
+	option dest_ip '203.165.31.152,122.197.254.136'
+	option family 'ipv4'
+	option use_policy 'wan2_only'
+
+config rule 'default_rule'
+	option dest_ip '0.0.0.0/0'
+	option family 'ipv4'
+	option use_policy 'balanced'
+EOF
+
+
 # Remove leases that were made before the static DHCP settings
 rm -f /tmp/dhcp.leases
